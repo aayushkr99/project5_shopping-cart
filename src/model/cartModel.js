aedfcc7aa366c0dacb4ae711140f8607be1a0a84
aayushkr99 <ak858098@gmail.com>
2022-06-02 18:59:20 +0530

    revised

--- src/model/cartModel.js
+++ src/model/cartModel.js
@@ -20,7 +20,8 @@ const cartSchema = new mongoose.Schema({
             type: Number,
             required: true,
             minlength: 1
-        }
+        },
+        _id: false
     }], 
     totalPrice: {
         type: Number,
